c49936cc3814a5f7e5cbb94d99657a4d8dd67ed8
sobryan <sobryan@shazam.net>
2018-05-14 14:54:47 -0500

    Initial commit

---
+++ src/main/java/com/example/restappa/RestAppAApplication.java
@@ -0,0 +1,12 @@
+package com.example.restappa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAppAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAppAApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/restappa/RestAppAApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.restappa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestAppAApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
